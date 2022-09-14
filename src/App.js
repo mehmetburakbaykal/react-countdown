dbfc06a226faa85a2a21571a9e0ad727e3353fc7
Burak Baykal <mehmetburakbaykal1@gmail.com>
2022-09-14 22:09:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [hoursTimer, setHoursTimer] = useState("10");
+  const [minutesTimer, setMinutesTimer] = useState("10");
+  const [secondsTimer, setSecondsTimer] = useState("10");
+
+  let interval;
+
+  const startTimer = () => {
+    const countdownDate = new Date("September 09,2023 20:00:00").getTime();
+
+    console.log(Math.floor(countdownDate));
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countdownDate - now;
+
+      const hours = Math.floor(
+        (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+      console.log(hours);
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setHoursTimer(hours);
+        setMinutesTimer(minutes);
+        setSecondsTimer(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{hoursTimer}</span>
+      <span>Hours</span>
+      <span>{minutesTimer}</span>
+      <span>Minutes</span>
+      <span>{secondsTimer}</span>
+      <span>Seconds</span>
     </div>
   );
 }
